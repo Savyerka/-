59b32b8f151fcefc953fdc0e17941273c9161967
Savyerka <Tenkow1002@mail.ru>
2023-05-28 23:56:44 +0300

    nv

---
+++ 8/type3.h
@@ -0,0 +1,22 @@
+//
+// Created by чел on 28.05.2023.
+//
+
+#ifndef TIP_TYPE3_H
+#define TIP_TYPE3_H
+
+
+class type3 {
+private:
+    double C;
+public:
+    explicit type3(double c1);
+
+    void Get_answer();
+
+    void show();
+
+};
+
+
+#endif //TIP_TYPE3_H
---
+++ 8/type4.h
@@ -0,0 +1,21 @@
+//
+// Created by чел on 28.05.2023.
+//
+
+#ifndef TIP_TYPE4_H
+#define TIP_TYPE4_H
+
+
+class type4 {
+private:
+    double A, C;
+public:
+    type4(double a1, double c1);
+
+    void Get_answer();
+
+    void show();
+};
+
+
+#endif //TIP_TYPE4_H
